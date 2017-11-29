297127855b415b97024ae55c1cd80e8e351de145
timsun <timsun@firmstep.com>
2017-11-29 13:27:21 +1300

    Daemon-StreamdorScraper-006 Set the fouth parameter of PDO to set utf8mb4 character set and collate

--- classes/Common/IO.php
+++ classes/Common/IO.php
@@ -157,7 +157,7 @@ class IO
         }
 
         try {
-            $pdo = new \PDO($connection['dns'], $connection['username'], $connection['password'], array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $pdo = new \PDO($connection['dns'], $connection['username'], $connection['password'], array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"));
             $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
             $pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
             $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
